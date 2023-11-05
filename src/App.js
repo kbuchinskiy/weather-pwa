4503e3265a7b026a9daacfb87d613bfd9e07c89b
kbuchinskiy <kirillsmailbox@gmail.com>
2023-11-05 19:59:24 +0200

    feat: add recent cities and wrong city error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { fetchWeather } from './api/fetchWeather';
+import 'reset-css/reset.css';
 import './App.css';
-
+const LAST_CITIES_LENGTH = 5;
 const App = () => {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
+    const [recentCities, setRecentCities] = useState([]);
     const [isOffline, setIsOffline] = useState(!navigator.onLine);
+    const [isCityWrong, setIsCityWrong] = useState(false);
 
     const updateOnlineStatus = () => {
         setIsOffline(!navigator.onLine);
@@ -15,60 +18,103 @@ const App = () => {
         window.addEventListener('online',  updateOnlineStatus);
         window.addEventListener('offline', updateOnlineStatus);
 
+        const savedCities = JSON.parse(localStorage.getItem('recentCities')) || [];
+        setRecentCities(savedCities);
+
         return () => {
             window.removeEventListener('online', updateOnlineStatus);
             window.removeEventListener('offline', updateOnlineStatus);
         };
     }, []);
 
-    const search = async (e) => {
-        if(e.key === 'Enter') {
-            try {
-                const data = await fetchWeather(query);
-                setWeather(data);
-                setQuery('');
-            } catch (error) {
-                console.error("Failed to fetch weather", error);
+    const updateRecentCities = (city) => {
+        setRecentCities(prevCities => {
+            const updatedCities = [city, ...prevCities.filter(c => c !== city)];
+            if (updatedCities.length > LAST_CITIES_LENGTH) {
+                updatedCities.pop();
             }
+            localStorage.setItem('recentCities', JSON.stringify(updatedCities));
+            return updatedCities;
+        });
+    }
+
+    const getWeatherForCity = async (city) => {
+        setIsCityWrong(false);
+        try {
+            const data = await fetchWeather(city);
+            setWeather(data);
+            updateRecentCities(data.location.name);
+            setQuery(data.location.name);
+        } catch (error) {
+            setIsCityWrong(true);
+            console.error("Failed to fetch weather", error);
         }
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        await getWeatherForCity(query);
+    }
+
+    const handleRecentCityClick = async (city) => {
+        setQuery(city);
+        await getWeatherForCity(city);
+    }
+
+    const onQueryUpdate = (query) => {
+        setQuery(query);
+        setIsCityWrong(false);
     }
 
     return (
         <div className="main-container">
-            <form onSubmit={handleSubmit} action="" autocomplete="on">
-            <input
-                autocomplete="on"
-                type="text"
-                name="city"
-                className="search-input"
-                placeholder="Type city name..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyDown={search}
-            />
-            </form>
+            <div className="form-wrapper">
+
+                <form className="form" onSubmit={handleSubmit} action="" autoComplete="on">
+                    <input
+                        autoComplete="on"
+                        type="text"
+                        name="city"
+                        className={`search-input ${isCityWrong ? 'input-error' : ''}`}
+                        placeholder="Type city name..."
+                        value={query}
+                        onChange={(e) => onQueryUpdate(e.target.value)}
+                    />
+                    <button type="submit">go</button>
+                </form>
+                {isCityWrong && <p className="form-error">Wrong city name</p>}
+            </div>
 
             {isOffline && <div className="offline-message">You're currently offline, showing last cached results.</div>}
+            {(recentCities.length > 0 && !weather.location) && (
+                <div className="recent-cities">
+                    <h2>recent cities:</h2>
+                    <ul>
+                        {recentCities.map(city => (
+                            <li onClick={ () => handleRecentCityClick(city)} key={city}>{city}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
 
             {weather.location && (
-                <div className="result">
-                    <h2 className="result-location">
-                        <span>{weather.location.name}</span>
-                        <sup>{weather.location.country}</sup>
-                    </h2>
-                    <div className="result-temp">
-                        {Math.round(weather.current.temp_c)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="result-info">
-                        <img className="result-city-icon" src={weather.current.condition.icon} alt="weather icon"/>
-                        <p>{weather.current.condition.text}</p>
-                    </div>
-                </div>
+                <article className="weather-result">
+                    <header className="weather-location">
+                        <h2 className="weather-location-city">{weather.location.name}</h2>
+                        <p className="weather-location-country">{weather.location.country}</p>
+                    </header>
+                    <section className="weather-temp">
+                        <h3>temp: <strong>{Math.round(weather.current.temp_c)}<sup>&deg;C</sup></strong></h3>
+                        <h3>feels like: <strong>{Math.round(weather.current.temp_c)}<sup>&deg;C</sup></strong></h3>
+                    </section>
+                    <section className="weather-info">
+                        <figure>
+                            <img className="weather-icon" src={weather.current.condition.icon} alt="Weather icon"/>
+                            <figcaption>{weather.current.condition.text}</figcaption>
+                        </figure>
+                    </section>
+                </article>
+
             )}
         </div>
     );
